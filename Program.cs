4f064bde40692a8a02239e47f289a120846d736b
Wenderson <wenderson.droid@gmail.com>
2020-08-02 22:49:15 -0300

    modificação 22:48

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nplease What is your name?__");
+            Console.WriteLine("\nplease What is your name?");
             var name = Console.ReadLine();
             var date = DateTime.Now;
             Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}! ");
